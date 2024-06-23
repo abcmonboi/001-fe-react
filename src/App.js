a4bb3dcf45b87348668d16cc04a995974200a9c7
ABCoder <gibb6699@gmail.com>
2024-06-23 18:26:42 +0700

    Navbar add

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+function App() {
+  return (
+    <div className="App">
+      <Header />
+          Learn React
+    </div>
+  );
+}
+
+export default App;
